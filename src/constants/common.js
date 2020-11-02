78a756b9a780b4c6e7b596d8a438591c8cc5c802
vikram <vikramk9852@gmail.com>
2020-11-02 18:20:18 +0530

    changed base-api-url pointing to remote

--- src/constants/common.js
+++ src/constants/common.js
@@ -1,4 +1,4 @@
-export const BASE_API_URL = "http://localhost:5000";
+export const BASE_API_URL = "http://34.68.6.169:5000";
 export const SEARCH_ENDPOINT = "search";
 export const ADD_USER_ENDPOINT = "addUser";
 export const GET_ALL_USERS_ENDPOINT = "getAllUsers";
